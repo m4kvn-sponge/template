cfee96fda5a2c61e95634040665a5903d7f7e676
m4kvn <fun.masahiro@gmail.com>
2018-11-19 03:18:25 +0900

    Add onServerStart Listener

--- src/main/kotlin/com/m4kvn/spongetemplate/Main.kt
+++ src/main/kotlin/com/m4kvn/spongetemplate/Main.kt
@@ -1,6 +1,8 @@
 package com.m4kvn.spongetemplate
 
 import org.slf4j.Logger
+import org.spongepowered.api.event.Listener
+import org.spongepowered.api.event.game.state.GameStartedServerEvent
 import org.spongepowered.api.plugin.Plugin
 import javax.inject.Inject
 
@@ -13,4 +15,9 @@ class Main {
 
     @Inject
     lateinit var logger: Logger
+
+    @Listener
+    fun onServerStart(event: GameStartedServerEvent) {
+        logger.debug("Sponge Server Plugin on Start.")
+    }
 }
